1aff49f0449e03764230f59252be7d17c26c45cc
root <root@vps381596.ovh.net>
2018-11-27 17:36:47 +0100

    renamed index to server + cleaned train.py

--- train.py
+++ train.py
@@ -1,20 +1,14 @@
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.core import Dense
 from keras.optimizers import SGD
 import numpy as np
 
-def main():
-    X = np.array([[0,0],[0,1],[1,0],[1,1]])
-    Y = np.array([[0],[1],[1],[0]])
+X = np.array([[0,0],[0,1],[1,0],[1,1]])
+Y = np.array([[0],[1],[1],[0]])
 
-    model = Sequential()
-    model.add(Dense(8, input_dim=2, activation='tanh'))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer=SGD(lr=0.1))
-    model.fit(X, Y, batch_size=1, nb_epoch=1000)
-    model.summary()
-
-    model.save('xor_model')
-
-if __name__=='__main__':
-    main()
+model = Sequential()
+model.add(Dense(8, input_dim=2, activation='tanh'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer=SGD(lr=0.1))
+model.fit(X, Y, batch_size=1, nb_epoch=1000)
+model.save('xor_model')
