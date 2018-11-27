ceaea492474cff1b766d35bb17faf45ff7e09c98
root <root@vps381596.ovh.net>
2018-11-27 17:28:06 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from keras.models import load_model
+import tensorflow as tf
+import numpy as np
+import flask
+
+app = Flask(__name__)
+graph = tf.get_default_graph()
+model = load_model('xor_model')
+
+@app.route('/')
+def hello():
+    data = {'result': 'Hello World!'}
+    return flask.jsonify(data)
+
+@app.route('/predict')
+def predict():
+    input = request.args.getlist('x', type=float)
+    with graph.as_default():
+        result = model.predict(np.array([input])).tolist()
+        data = {'result': result}
+        return flask.jsonify(data)
+
+app.run(host='0.0.0.0', debug=False)
