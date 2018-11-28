ddcf4ee4597720badb9acc68bb49807027d33728
root <root@vps381596.ovh.net>
2018-11-28 17:06:44 +0100

    added comments

--- train.py
+++ train.py
@@ -3,12 +3,16 @@ from keras.layers.core import Dense
 from keras.optimizers import SGD
 import numpy as np
 
+# training data
 X = np.array([[0,0],[0,1],[1,0],[1,1]])
 Y = np.array([[0],[1],[1],[0]])
 
+# simple 3 layer neural network
 model = Sequential()
 model.add(Dense(8, input_dim=2, activation='tanh'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer=SGD(lr=0.1))
 model.fit(X, Y, batch_size=1, nb_epoch=1000)
+
+# save model and weights to file
 model.save('xor_model')
