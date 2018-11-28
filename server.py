d6e9e93e96c377a44f523aba6ae0d88574d140c9
root <root@vps381596.ovh.net>
2018-11-28 17:07:16 +0100

    added comments + simplified code

--- server.py
+++ server.py
@@ -5,20 +5,30 @@ import numpy as np
 import flask
 
 app = Flask(__name__)
-graph = tf.get_default_graph()
 model = load_model('xor_model')
+graph = tf.get_default_graph()
 
+# handle request (GET by default)
 @app.route('/')
 def hello():
     data = {'result': 'Hello World!'}
     return flask.jsonify(data)
 
-@app.route('/predict')
+# request model prediction
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    input = request.args.getlist('x', type=float)
+    if request.method == 'GET':
+        a = request.args['a']
+        b = request.args['b']
+    elif request.method == 'POST':
+        a = request.form['a']
+        b = request.form['b']
+
+    # Required because of a bug in Keras when using tensorflow graph cross threads
     with graph.as_default():
-        result = model.predict(np.array([input]))[0].tolist()
+        result = model.predict(np.array([[a,b]]))[0].tolist()
         data = {'result': result}
         return flask.jsonify(data)
 
-app.run(host='0.0.0.0', debug=False)
+# start Flask server
+app.run(host='0.0.0.0', port=5000, debug=False)
